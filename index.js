a9756be66d7d5a8db2affca1aca36c3d7dd1905b
Vikas Kumar Flutter <vikaskumarflutter@gmail.com>
2021-07-05 12:52:05 +0530

    first website using node js backend

--- index.js
+++ index.js
@@ -28,15 +28,71 @@ const url = require("url");
 /*--------------------------*/
 //Server
 
-const port = 3000;
+const port = 8000;
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
+  const { query, pathname } = url.parse(req.url,true);
 
-  if (url == "/" || url == "/overview") {
-    res.end("This is the OVERVIEW");
-  } else if (url == "/product") {
-    res.end("This is the PRODUCT");
+  //Overview Page
+  if (pathname == "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+
+    //Product Page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct,product);
+    res.end(output);
+
+    //Api
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    //Not Found Page
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-header": "hello world!",
+    });
+    res.end("<h2>Page not found</h2>");
   }
 });
 
