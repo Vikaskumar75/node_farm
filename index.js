37e1c15c50dc6de503e03cc947d31453b89ca5a2
Vikas Kumar Flutter <vikaskumarflutter@gmail.com>
2021-07-17 22:47:44 +0530

    added modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const { URL } = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 /*--------------------------*/
 //Filesystem
@@ -29,6 +30,7 @@ const { URL } = require("url");
 //Server
 
 const port = 8000;
+const localHost = "https://127.0.0.1:";
 
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
@@ -46,26 +48,10 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
-
 const server = http.createServer((req, res) => {
-  const myUrl = new URL( req.url ,`https://127.0.0.1:8000`);
+  const myUrl = new URL(req.url, `${localHost}${port}`);
   const pathname = myUrl.pathname;
-  const query = myUrl.searchParams.get('id');
+  const query = myUrl.searchParams.get("id");
 
   //Overview Page
   if (pathname == "/" || pathname === "/overview") {
